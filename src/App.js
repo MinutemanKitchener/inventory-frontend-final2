60af419ac9920b322934c14b539f46881870c731
MinutemanKitchener <print@minuteman.on.ca>
2025-03-25 11:53:45 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Inventory App</h1>
+      <p>This version works from iPhone and builds on Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
